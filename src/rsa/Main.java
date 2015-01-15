895c47c57a080ee262ba281478b147ef1bb63101
David Hughes <hughes.david0@gmail.com>
2015-01-14 20:30:28 -0500

    Making main Program

---
+++ src/rsa/Main.java
@@ -0,0 +1,21 @@
+package rsa;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    RSA rsa = new RSA(1024,"/Users/hughdw11/code/CSC222/rsa");
+    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Public Key: "+ rsa.publicKey + "\n");
+    
+    System.out.println("Encrypted Message: ");
+    BigInteger encrpt = scan.nextBigInteger();
+    BigInteger decrypt = rsa.decrpyt(encrpt);
+    
+    System.out.println("\n\n Message Decrypts to: "+ decrypt);
+  }
+
+}
