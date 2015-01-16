118115098b9bb3890e9c13b3b593ad00ea26313f
David Hughes <hughes.david0@gmail.com>
2015-01-16 09:12:12 -0500

    Testing Pub Priv Key

--- src/rsa/Main.java
+++ src/rsa/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    RSA rsa = new RSA(1024,"/Users/hughdw11/code/CSC222/rsa");
+    RSA rsa = new RSA(1024,"/Users/hughdw11/code/CSC222/rsa/kets.txt");
     
     Scanner scan = new Scanner(System.in);
     System.out.println("Public Key: "+ rsa.publicKey + "\n");
